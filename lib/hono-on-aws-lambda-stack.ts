dd8a8e07e0f3dc8edbf2b622308f36f8c76394bf
ve1997 <83203852+ve1997@users.noreply.github.com>
2024-06-27 02:52:16 +0900

    feat: Add Hono API endpoint with AWS Lambda integration

--- lib/hono-on-aws-lambda-stack.ts
+++ lib/hono-on-aws-lambda-stack.ts
@@ -1,4 +1,26 @@
 import * as cdk from "aws-cdk-lib";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as apigw from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import type { Construct } from "constructs";
 
-export class HonoOnAwsLambdaStack extends cdk.Stack {}
+export class HonoOnAwsLambdaStack extends cdk.Stack {
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
+
+		const fn = new NodejsFunction(this, "lambda", {
+			entry: "lambda/index.ts",
+			handler: "handler",
+			runtime: lambda.Runtime.NODEJS_20_X,
+		});
+		fn.addFunctionUrl({
+			authType: lambda.FunctionUrlAuthType.NONE,
+		});
+		new apigw.LambdaRestApi(this, "hono-on-aws-lambda", {
+			handler: fn,
+			deployOptions: {
+				stageName: "dev",
+			},
+		});
+	}
+}
